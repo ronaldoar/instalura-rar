adeee06fd85ad65858ac1407fca1a3dc54b885fd
= <ronaldo.alves.rios@gmail.com>
2021-02-15 16:37:13 -0300

    Ajuste no Button

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,14 +1,14 @@
-import styled, { css } from 'styled-components';
+import styled, {css} from 'styled-components';
 import get from 'lodash/get';
 
 const ButtonGhost = css`
-    color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
-    background-color: transparent;
+    background:transparent;
+    color: ${(props) => get(props.theme,`colors.${props.variant}.color`)};
 `;
 
 const ButtonDefault = css`
-    color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
-    background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+    background-color: ${(props) => get(props.theme,`colors.${props.variant}.color`)};
+    color: ${(props) => get(props.theme,`colors.${props.variant}.contrastText`)};
 `;
 
 export const Button = styled.button`
@@ -16,10 +16,16 @@ export const Button = styled.button`
     cursor: pointer;
     padding: 12px 26px;
     font-weight: bold;
-    opacity: 1;
-    transition: opacity ${({ theme }) => theme.transition};
-    border-radius: ${({ theme }) => theme.borderRadius};
-    ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
+    opacity:1;
+    border-radius: 8px;
+    transition: ${({theme}) => theme.transition};
+    border-radius: ${({theme}) => theme.borderRadius};
+    ${function(props){
+        if (props.ghost) {
+            return ButtonGhost;
+        }
+        return ButtonDefault;
+    }}
     &:hover,
     &:focus {
         opacity: .5;
